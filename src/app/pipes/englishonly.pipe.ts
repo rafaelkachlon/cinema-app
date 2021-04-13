f6cf6c12785080f45cfdc0159142f541f49871ff
Rafael <rafaelkachlon@gmail.com>
2021-04-13 22:06:56 +0300

    set english only pipe to accept numbers

--- src/app/pipes/englishonly.pipe.ts
+++ src/app/pipes/englishonly.pipe.ts
@@ -5,6 +5,6 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 export class EnglishOnlyPipe implements PipeTransform {
   transform(value: any, ...args): any {
-    return value.replace(/[^a-z-A-Z ]/g, '');
+    return value.replace(/[^a-z-A-Z 0-9]/g, '');
   }
 }
